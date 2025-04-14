d060ffcb92ece12ad78553087768dcf694bd1f26
sourceshipper <154885181+sourceshipper@users.noreply.github.com>
2025-04-14 17:20:16 +0800

    pipeline

--- bump-version.sh
+++ bump-version.sh
@@ -7,10 +7,10 @@ VERSION=$(cat version.txt)
 IFS='.' read -ra ADDR <<< "$VERSION"
 
 ## Bump the patch version. You can modify this logic as per your versioning strategy.
-ADDR[3]=$((ADDR[3]+1))
+#ADDR[3]=$((ADDR[3]+1))
 
 # Use the GitHub Actions workflow run number for the last part of the version
-#ADDR[3]=$GITHUB_RUN_NUMBER
+ADDR[3]=$GITHUB_RUN_NUMBER
 
 # Construct new version
 NEW_VERSION="${ADDR[0]}.${ADDR[1]}.${ADDR[2]}.${ADDR[3]}"
